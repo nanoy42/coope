f0aa02eeb47cd18243a68375126a9476d4d64db8
nanoy <me@nanoy.fr>
2018-12-23 21:48:49 +0100

    Add search bar on profile admin

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,10 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from .models import School, Profile, CotisationHistory
 
+class ProfileAdmin(SimpleHistoryAdmin):
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+
 admin.site.register(Permission, SimpleHistoryAdmin)
 admin.site.register(School, SimpleHistoryAdmin)
-admin.site.register(Profile, SimpleHistoryAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(CotisationHistory, SimpleHistoryAdmin)
