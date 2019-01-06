0fa1ab34eae6e8b12d167a01d3124e53d347661e
nanoy <me@nanoy.fr>
2019-01-06 15:57:02 +0100

    Fix 3.2.1

---
+++ django_tex/engine.py
@@ -0,0 +1,11 @@
+
+from django.template.backends.jinja2 import Jinja2
+
+class TeXEngine(Jinja2):
+    app_dirname = 'templates'
+
+    def __init__(self, params):
+        default_environment = 'django_tex.environment.environment'
+        if 'environment' not in params['OPTIONS'] or not params['OPTIONS']['environment']:
+            params['OPTIONS']['environment'] = default_environment
+        super().__init__(params)
---
+++ django_tex/views.py
@@ -0,0 +1,17 @@
+
+from django.http import HttpResponse
+
+from django_tex.core import compile_template_to_pdf
+
+class PDFResponse(HttpResponse):
+
+    def __init__(self, content, filename=None):
+        super(PDFResponse, self).__init__(content_type='application/pdf')
+        self['Content-Disposition'] = 'filename="{}"'.format(filename)
+        self.write(content)
+
+
+def render_to_pdf(request, template_name, context=None, filename=None):
+    # Request is not needed and only included to make the signature conform to django's render function
+    pdf = compile_template_to_pdf(template_name, context)
+    return PDFResponse(pdf, filename=filename)
